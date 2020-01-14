cc172ba0a48b603c2504bc5f2c41945a3437f216
rivofasutav <rivofaustav.rv@gmail.com>
2020-01-14 14:34:47 +0700

    add views pengumuman list static

---
+++ application/views/admin/pengumuman/list.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<?php $this->load->view("admin/_partials/head.php") ?>
+</head>
+
+<body id="page-top">
+
+	<?php $this->load->view("admin/_partials/navbar.php") ?>
+	<div id="wrapper">
+
+		<?php $this->load->view("admin/_partials/sidebar.php") ?>
+
+		<div id="content-wrapper">
+
+			<div class="container-fluid">
+
+				<?php $this->load->view("admin/_partials/breadcrumb.php") ?>
+
+				<!-- DataTables -->
+				<div class="card mb-3">
+					<div class="card-header">
+						<a href="<?php echo site_url('admin/pengumuman/add') ?>"><i class="fas fa-plus"></i> Add New</a>
+					</div>
+					<div class="card-body">
+
+						<div class="table-responsive">
+							<table class="table table-hover" id="dataTable" width="100%" cellspacing="0">
+								<thead>
+									<tr>
+										<th>Judul</th>
+										<th>Isi</th>
+										<!-- <th>Photo</th>
+										<th>Description</th>
+										<th>Action</th> -->
+									</tr>
+								</thead>
+								<tbody>
+									<?php foreach ($pengumuman as $pengumuman): ?>
+									<tr>
+										<td width="150">
+											<?php echo $pengumuman->judul ?>
+										</td>
+										<td>
+											<?php echo $pengumuman->isi ?>
+										</td>
+										<!-- <td>
+											<img src="<?php echo base_url('upload/pengumuman/'.$pengumuman->image) ?>" width="64" />
+										</td> -->
+										<!-- <td class="small">
+                                            <?php echo substr($pengumuman->description, 0, 120) ?>...
+                                        </td> -->
+										<td width="250">
+											<a href="<?php echo site_url('admin/pengumuman/edit/'.$pengumuman->pengumuman_id) ?>"
+											 class="btn btn-small"><i class="fas fa-edit"></i> Edit</a>
+											<a onclick="deleteConfirm('<?php echo site_url('admin/pengumuman/delete/'.$pengumuman->pengumuman_id) ?>')"
+											 href="#!" class="btn btn-small text-danger"><i class="fas fa-trash"></i> Hapus</a>
+										</td>
+									</tr>
+									<?php endforeach; ?>
+
+								</tbody>
+							</table>
+						</div>
+					</div>
+				</div>
+
+			</div>
+			<!-- /.container-fluid -->
+
+			<!-- Sticky Footer -->
+			<?php $this->load->view("admin/_partials/footer.php") ?>
+
+		</div>
+		<!-- /.content-wrapper -->
+
+	</div>
+	<!-- /#wrapper -->
+
+
+	<?php $this->load->view("admin/_partials/scrolltop.php") ?>
+	<?php $this->load->view("admin/_partials/modal.php") ?>
+
+	<?php $this->load->view("admin/_partials/js.php") ?>
+
+</body>
+
+</html>
