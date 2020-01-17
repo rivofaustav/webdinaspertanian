e2bccb086fc9499665d3cab328ec218edbdaa07f
rivofasutav <rivofaustav.rv@gmail.com>
2020-01-17 11:58:35 +0700

    add controlers and views login

--- application/controllers/admin/Overview.php
+++ application/controllers/admin/Overview.php
@@ -4,6 +4,9 @@ class Overview extends CI_Controller {
     public function __construct()
     {
 		parent::__construct();
+		//login access
+		$this->load->model("user_model");
+		if($this->user_model->isNotLogin()) redirect(site_url('admin/login'));
 	}
 
 	public function index()
