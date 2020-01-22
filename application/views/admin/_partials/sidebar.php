bb764cf48d5cdd7f901b734d82cf7a12560ab612
febtrianiari <febtria.niari@gmail.com>
2020-01-22 21:22:57 +0700

    sidebar galeri

--- application/views/admin/_partials/sidebar.php
+++ application/views/admin/_partials/sidebar.php
@@ -17,6 +17,17 @@
             <a class="dropdown-item" href="<?php echo site_url('admin/pengumuman') ?>">List pengumuman</a>
         </div>
     </li>
+    <li class="nav-item dropdown <?php echo $this->uri->segment(2) == 'galeri' ? 'active': '' ?>">
+        <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true"
+            aria-expanded="false">
+            <i class="fas fa-fw fa-boxes"></i>
+            <span>Galeri</span>
+        </a>
+        <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+            <a class="dropdown-item" href="<?php echo site_url('admin/galeri/add') ?>">Galeri</a>
+            <a class="dropdown-item" href="<?php echo site_url('admin/galeri') ?>">List Galeri</a>
+        </div>
+    </li>
     <li class="nav-item">
         <a class="nav-link" href="#">
             <i class="fas fa-fw fa-users"></i>
